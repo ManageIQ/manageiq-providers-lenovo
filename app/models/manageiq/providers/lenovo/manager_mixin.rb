f6333b766125445243ca0a629169ec2cf6ac1a71
Julian Cheal <jcheal@redhat.com>
2016-08-30 20:19:45 +0100

    Adding connection details to provider

--- app/models/manageiq/providers/lenovo/manager_mixin.rb
+++ app/models/manageiq/providers/lenovo/manager_mixin.rb
@@ -7,10 +7,14 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
   #
   # Connections
   #
-
   def connect(options = {})
+    raise "no credentials defined" if missing_credentials?(options[:auth_type])
 
-    self.class.raw_connect()
+    username = options[:user] || authentication_userid(options[:auth_type])
+    password = options[:pass] || authentication_password(options[:auth_type])
+    host     = options[:host]
+
+    self.class.raw_connect(username, password, host)
   end
 
   def translate_exception(err)
@@ -23,8 +27,13 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
     #
     # Connections
     #
-
-    def raw_connect()
+    def raw_connect(username, password, host)
+      require 'xclarity_client'
+      XClarityClient::Configuration.new(
+        :username => username,
+        :password => password,
+        :host     => host
+      )
     end
 
     #
