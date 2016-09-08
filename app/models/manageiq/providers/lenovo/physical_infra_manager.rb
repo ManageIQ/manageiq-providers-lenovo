dd57c89e19b58e8c1eb3faa94b59366516affecf
Walter Alves <walter.arruda.alves@gmail.com>
2016-09-08 10:15:19 -0300

    Changing inheritance of Lenovo::PhysicalManager

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -1,6 +1,7 @@
 class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
 end
 
-class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
+#TODO Lenovo::PhysucalInfraManager have to extends ManageIQ::Providers::PhysicalInfraManager
+class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::InfraManager
   include ManageIQ::Providers::Lenovo::ManagerMixin
 end
