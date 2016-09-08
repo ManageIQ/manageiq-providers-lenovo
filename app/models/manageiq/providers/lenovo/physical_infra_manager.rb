0a5e78be02ad653436d402d1159c43564787c745
Tercio de Melo <terciodemelo@gmail.com>
2016-09-08 15:16:57 -0300

    Creating Lenovo Physical Infrastructure Manager placeholder

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -1,3 +1,6 @@
+class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
+end
+
 class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
   include ManageIQ::Providers::Lenovo::ManagerMixin
 end
