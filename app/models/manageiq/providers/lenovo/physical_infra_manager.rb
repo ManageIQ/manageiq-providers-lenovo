10e3728c4d70448f114c6cef213fd7cb46d46c13
Walter Alves <walter.arruda.alves@gmail.com>
2016-09-09 02:13:39 -0300

    Manager description and inheritance changed

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -1,6 +1,6 @@
 class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
 end
 
-class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
+class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::InfraManager
   include ManageIQ::Providers::Lenovo::ManagerMixin
 end
