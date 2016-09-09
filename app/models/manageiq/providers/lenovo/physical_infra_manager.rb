4ac68fc8998b2d28de66300519ed8efc4f5da04a
Walter Alves <walter.arruda.alves@gmail.com>
2016-09-09 12:37:35 -0300

    Changing inheritance temporary.

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -1,7 +1,7 @@
-class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
-end
+#class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
+#end
 
-class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
+class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::InfraManager
   include ManageIQ::Providers::Lenovo::ManagerMixin
 
   def self.ems_type
