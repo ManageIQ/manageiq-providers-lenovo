f0471092a2c56c19357cc9312bfc05d03fc86ad8
Walter Alves <walter.arruda.alves@gmail.com>
2016-09-16 08:26:52 -0300

    Changing RefreshWoerker inherintance and implmeentation

--- app/models/manageiq/providers/lenovo/physical_infra_manager/refresh_worker.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/refresh_worker.rb
@@ -1,8 +1,12 @@
-class ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshWorker < ManageIQ::Providers::BaseManager::RefreshWorker
+class ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshWorker < ::MiqEmsRefreshWorker
   require_nested :Runner
 
   def self.ems_class
     ManageIQ::Providers::Lenovo::PhysicalInfraManager
+  end
+
+  def self.settings_name
+    :ems_refresh_worker_lenovo_physical_infra
   end
 
 end
