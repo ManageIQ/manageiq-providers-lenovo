138b507bf6559163447cf43d67cf980a8162eebb
Walter Alves <walter.arruda.alves@gmail.com>
2016-09-16 10:26:27 -0300

    Refresh Logging.

--- app/models/manageiq/providers/lenovo/physical_infra_manager/refresher.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/refresher.rb
@@ -3,6 +3,8 @@ module ManageIQ::Providers::Lenovo
     include ::EmsRefresh::Refreshers::EmsRefresherMixin
 
     def parse_legacy_inventory(ems)
+
+      log_header = "MIQ_LENOVO(#{self.class.name}.#{__method__} Calling for [#{@ems.name}])"
       ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshParser.ems_inv_to_hashes(ems, refresher_options)
     end
 
