a4f18184db11369af5e7a0c066ef185d8c91114c
Julian Cheal <jcheal@redhat.com>
2016-11-02 12:54:13 +0000

    Changed lenovo_ph_infra to lenovo_physical_infra_manager

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -1,11 +1,8 @@
-#class ManageIQ::Providers::PhysicalInfraManager < ManageIQ::Providers::InfraManager
-#end
-
 class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::PhysicalInfraManager
   include ManageIQ::Providers::Lenovo::ManagerMixin
 
   def self.ems_type
-    @ems_type ||= "lenovo_ph_infra".freeze
+    @ems_type ||= "lenovo_physical_infra_manager".freeze
   end
 
   def self.description
