534ad4dab1fa287777e931a9a5de818c0a9595e4
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-12-05 11:19:26 -0300

    [EventCatcher] WIP - Initial integrator commit

--- app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
@@ -6,27 +6,29 @@ module ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventParser
     #
     # _log.debug("#{log_header}event: [#{event["configurationItem"]["resourceType"]} - " \
     #            "#{event["configurationItem"]["resourceId"]}]")
-    #
-    # event_hash = {
-    #   :event_type => event["eventType"],
-    #   :source     => "LENOVO",
-    #   :message    => event["configurationItemDiff"],
-    #   :timestamp  => event["notificationCreationTime"],
-    #   :full_data  => event,
-    #   :ems_id     => ems_id
-    # }
-    #
+
+    
+
+    #TODO: Here's where we'll turn event in a hash
+    event_hash = {
+      :event_type => event.localLogID,
+      :source     => "LXCA",
+      :message    => event.msg,
+      :timestamp  => event.eventDate,
+      :full_data  => event,
+      :ems_id     => ems_id
+    }
+
     # event_hash[:vm_ems_ref]                = parse_vm_ref(event)
     # event_hash[:availability_zone_ems_ref] = parse_availability_zone_ref(event)
-    # event_hash
+    event_hash
   end
 
   def self.parse_vm_ref(event)
-
     #TODO: Investigating what this function do.
-    # resource_type = event["configurationItem"]["resourceType"]
-    # # other ways to find the VM?
-    # event.fetch_path("configurationItem", "resourceId") if resource_type == "Aws::EC2::Instance"
+    resource_type = event["configurationItem"]["resourceType"]
+    # other ways to find the VM?
+    event.fetch_path("configurationItem", "resourceId") if resource_type == "Aws::EC2::Instance"
   end
 
   def self.parse_availability_zone_ref(event)
