8e7aac6e6c7a583dc34bc2b602379b1436be2548
Odravison Amaral <odravison.amaral@dce.ufpb.br>
2016-12-16 15:12:02 -0300

    [EventResources] Changes the way that Event fulldata is saved
    This branch changes the fulldata of event to hash on event_parser.rb file

--- app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
@@ -8,7 +8,7 @@ module ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventParser
       :timestamp     => event.timeStamp,
       :lxca_severity => event.severity,
       :lxca_cn       => event.cn,
-      :full_data     => event,
+      :full_data     => event.to_hash,
       :ems_id        => ems_id
     }
 
