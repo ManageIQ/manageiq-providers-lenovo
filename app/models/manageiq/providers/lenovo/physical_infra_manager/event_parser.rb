6480b7e64d5696d8deeb525bf6977328ce0c3423
Andrey Menezes <andrey.menezes@gmail.com>
2017-03-30 17:44:19 -0300

    Add physical_server_id to the event hash

--- app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/event_parser.rb
@@ -1,16 +1,16 @@
 module ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventParser
   def self.event_to_hash(event, ems_id)
     event_hash = {
-      :event_type    => event.eventID,
-      :source        => "LenovoXclarity",
-      :message       => event.msg,
-      :timestamp     => event.timeStamp,
-      :lxca_severity => event.severity,
-      :lxca_cn       => event.cn,
-      :full_data     => event.to_hash,
-      :ems_id        => ems_id
+      :event_type           => event.eventID,
+      :source               => "LenovoXclarity",
+      :physical_server_id   => event.componentID,
+      :message              => event.msg,
+      :timestamp            => event.timeStamp,
+      :lxca_severity        => event.severity,
+      :lxca_cn              => event.cn,
+      :full_data            => event.to_hash,
+      :ems_id               => ems_id
     }
-
     event_hash
 
   end
