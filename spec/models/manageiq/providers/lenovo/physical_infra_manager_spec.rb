e6dcf7463eaa443c3bf1f8b883e679a387a8b5d7
skovic <skovic@lenovo.com>
2017-04-17 11:02:30 -0400

    Update spec tests

--- spec/models/manageiq/providers/lenovo/physical_infra_manager_spec.rb
+++ spec/models/manageiq/providers/lenovo/physical_infra_manager_spec.rb
@@ -1,13 +1,28 @@
 require 'xclarity_client'
 
 describe ManageIQ::Providers::Lenovo::PhysicalInfraManager do
-
-  pending "Must test authentication verification"
-
-  pending "Must test discovery"
-
   before :all do
-    @auth = { user: 'admin', pass: 'smartvm', host: 'localhost' }
+    @auth = { :user => 'admin', :pass => 'smartvm', :host => 'localhost' }
+  end
+
+  it 'will execute discover successfully' do
+    result = described_class.new.class.discover(@auth[:user], @auth[:pass], @auth[:host])
+    expect(result).to eq([])
+  end
+
+  it 'will execute discover_queue successfully' do
+    result = described_class.new.class.discover_queue(@auth[:user], @auth[:pass])
+    expect(result).to_not eq(nil)
+  end
+
+  it 'will execute discover_from_queue successfully' do
+    result = described_class.new.class.discover_from_queue(@auth[:user], @auth[:pass], @auth[:host])
+    expect(result).to_not eq(nil)
+  end
+
+  it 'will execute verify_credentials successfully' do
+    result = described_class.new.verify_credentials
+    expect(result).to eq(true)
   end
 
   it 'ems_type should be lenovo_ph_infra' do
