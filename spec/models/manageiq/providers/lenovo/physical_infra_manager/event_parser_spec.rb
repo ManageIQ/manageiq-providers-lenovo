4ddd16ddfb17e1f083285f35799a6bb6537f044d
Andrey Menezes <andrey.menezes@gmail.com>
2017-05-17 15:40:35 -0300

    Change attribute name and add physical server id

--- spec/models/manageiq/providers/lenovo/physical_infra_manager/event_parser_spec.rb
+++ spec/models/manageiq/providers/lenovo/physical_infra_manager/event_parser_spec.rb
@@ -3,12 +3,13 @@ require 'xclarity_client'
 describe ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventParser do
   let(:event_attrs1) do
     {
-      :eventID   => 1,
-      :msg       => "This is a test event.",
-      :timeStamp => "2017-04-26T13:55:49.749552",
-      :severity  => "",
-      :cn        => "",
-      :to_hash   => ""
+      :eventID            => 1,
+      :source             => "LenovoXclarity",
+      :msg                => "This is a test event.",
+      :timeStamp          => "2017-04-26T13:55:49.749552",
+      :physical_server_id => "",
+      :to_hash            => "",
+      :ems_id             => 3
     }
   end
 
@@ -17,8 +18,9 @@ describe ManageIQ::Providers::Lenovo::PhysicalInfraManager::EventParser do
   it 'will parse events' do
     event_hash = described_class.event_to_hash(event1, 3)
     expect(event_hash[:event_type]).to eq(1)
+    expect(event_hash[:source]).to eq("LenovoXclarity")
     expect(event_hash[:message]).to eq("This is a test event.")
-    expect(event_hash[:timeStamp]).to eq("2017-04-26T13:55:49.749552")
+    expect(event_hash[:timestamp]).to eq("2017-04-26T13:55:49.749552")
     expect(event_hash[:ems_id]).to eq(3)
   end
 end
