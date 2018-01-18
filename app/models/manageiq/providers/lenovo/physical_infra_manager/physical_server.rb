6584f21e7d25c2b1205deaccf11ca03c081d53e0
Milan Zazrivec <mzazrivec@redhat.com>
2018-01-18 11:21:41 +0100

    Migrate model display names from locale/en.yml to plugin

--- app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
@@ -1,5 +1,9 @@
 module ManageIQ::Providers
   class Lenovo::PhysicalInfraManager::PhysicalServer < ::PhysicalServer
     include_concern 'RemoteConsole'
+
+    def self.display_name(number = 1)
+      n_('Physical Server (Lenovo)', 'Physical Servers (Lenovo)', number)
+    end
   end
 end
