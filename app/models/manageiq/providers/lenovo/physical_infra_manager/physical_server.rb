4f13cc5d68f11ef7bc5d9b31b277108f6df4bdd8
Caio Paes <caiocmpaes@gmail.com>
2018-04-17 17:14:56 -0300

    Remove Physical Server delegates to Asset Detail

--- app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/physical_server.rb
@@ -1,20 +1,5 @@
 module ManageIQ::Providers
   class Lenovo::PhysicalInfraManager::PhysicalServer < ::PhysicalServer
     include_concern 'RemoteConsole'
-
-    delegate :product_name,
-             :product_name=,
-             :manufacturer,
-             :manufacturer=,
-             :machine_type,
-             :machine_type=,
-             :model,
-             :model=,
-             :serial_number,
-             :serial_number=,
-             :field_replaceable_unit,
-             :field_replaceable_unit=,
-             :to        => :asset_detail,
-             :allow_nil => true
   end
 end
