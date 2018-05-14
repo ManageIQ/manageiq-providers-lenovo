172a53ef0c62328dab3c225bec356c9ed96979d5
Caio Paes <caiocmpaes@gmail.com>
2018-05-14 18:06:39 -0300

    Removing unused refresh_helper_methods
    Some class properties were not necessary in `refresh_parser`. RefreshHelperMethods also became unnecessary.

--- app/models/manageiq/providers/lenovo/physical_infra_manager/refresher.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/refresher.rb
@@ -7,7 +7,7 @@ module ManageIQ::Providers::Lenovo
       log_header = "MIQ_LENOVO(#{self.class.name}.#{__method__} Calling for [#{ems.name}])"
       $log.info("#{log_header}")
 
-      ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshParser.ems_inv_to_hashes(ems, refresher_options)
+      ManageIQ::Providers::Lenovo::PhysicalInfraManager::RefreshParser.new(ems).ems_inv_to_hashes
     end
   end
 end
