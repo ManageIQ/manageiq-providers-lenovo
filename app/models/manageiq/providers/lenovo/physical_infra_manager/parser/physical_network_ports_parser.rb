dc26c447d6176fca15ffddb08d4c3a295b40b5a9
Caio Paes <caiocmpaes@gmail.com>
2018-05-15 17:28:02 -0300

    Switches ports are different from server ports. Different parsers then.
    The PhysicalNetworkPortsParser had two public methods. One for parsing switches ports and other for parsing server network ports. There was no reuse or cause for them to be together in a single class.

--- app/models/manageiq/providers/lenovo/physical_infra_manager/parser/physical_network_ports_parser.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/parser/physical_network_ports_parser.rb
@@ -1,12 +1,6 @@
 module ManageIQ::Providers::Lenovo
   class PhysicalInfraManager::Parser::PhysicalNetworkPortsParser < PhysicalInfraManager::Parser::ComponentParser
     class << self
-      # Mapping between fields inside [Hash] Physical Switch Port to a [Hash] with symbols as keys
-      PHYSICAL_SWITCH_PORT = {
-        :peer_mac_address => 'peerMacAddress',
-        :vlan_key         => 'PVID',
-      }.freeze
-
       #
       # Mounts the ports from Network Device
       #
@@ -19,16 +13,6 @@ module ManageIQ::Providers::Lenovo
         end
 
         parsed_ports.uniq { |port| port[:port_name] }
-      end
-
-      #
-      # Mounts the Physical Switches ports
-      #
-      # @param [XClarityClient::Switch] physical_switch - The switch to have
-      #   it ports parsed
-      #
-      def parse_physical_switch_ports(physical_switch)
-        physical_switch.ports&.map { |port| parse_switch_port(port) }
       end
 
       private
@@ -44,15 +28,6 @@ module ManageIQ::Providers::Lenovo
           parsed_logical_port.merge!(parsed_physical_port)
           parsed_logical_port
         end
-      end
-
-      def parse_switch_port(port)
-        result = parse(port, PHYSICAL_SWITCH_PORT)
-        result.merge(
-          :port_name    => port['portName'].presence || port['port'],
-          :port_type    => 'physical_port',
-          :vlan_enabled => port['PVID'].present?
-        )
       end
 
       def parse_physical_port(port)
