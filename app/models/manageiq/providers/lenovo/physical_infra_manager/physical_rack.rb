90f898247a3018649f048637d46901d460941a3f
Milan Zazrivec <mzazrivec@redhat.com>
2018-06-12 16:53:58 +0200

    Add display name for physical rack

--- app/models/manageiq/providers/lenovo/physical_infra_manager/physical_rack.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/physical_rack.rb
@@ -1,4 +1,7 @@
 module ManageIQ::Providers
   class Lenovo::PhysicalInfraManager::PhysicalRack < ::PhysicalRack
+    def self.display_name(number = 1)
+      n_('Physical Rack (Lenovo)', 'Physical Racks (Lenovo)', number)
+    end
   end
 end
