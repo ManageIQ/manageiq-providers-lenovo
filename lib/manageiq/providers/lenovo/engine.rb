e5770eb4f9c3126d6578be759ac62069775182a3
Milan Zazrivec <mzazrivec@redhat.com>
2018-08-20 15:42:30 +0200

    Add plugin display name

--- lib/manageiq/providers/lenovo/engine.rb
+++ lib/manageiq/providers/lenovo/engine.rb
@@ -3,6 +3,10 @@ module ManageIQ
     module Lenovo
       class Engine < ::Rails::Engine
         isolate_namespace ManageIQ::Providers::Lenovo
+
+        def self.plugin_name
+          _('Lenovo Provider')
+        end
       end
     end
   end
