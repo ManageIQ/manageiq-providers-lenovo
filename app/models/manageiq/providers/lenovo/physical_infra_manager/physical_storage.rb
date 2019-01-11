539d888ceadbdc5f3612c3563dd6f219b7c12385
Milan Zazrivec <mzazrivec@redhat.com>
2019-01-11 12:48:18 +0100

    Add display name for Physical Storage

--- app/models/manageiq/providers/lenovo/physical_infra_manager/physical_storage.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager/physical_storage.rb
@@ -1,4 +1,7 @@
 module ManageIQ::Providers
   class Lenovo::PhysicalInfraManager::PhysicalStorage < ::PhysicalStorage
+    def self.display_name(number = 1)
+      n_('Physical Storage (Lenovo)', 'Physical Storages (Lenovo)', number)
+    end
   end
 end
