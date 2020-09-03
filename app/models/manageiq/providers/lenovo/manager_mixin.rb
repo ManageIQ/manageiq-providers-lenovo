2814ea4652e27c4fe522126fcb5d402fc4679097
Dávid Halász <dhalasz@redhat.com>
2020-09-03 18:31:40 +0200

    Explicitly add missing IDs into DDF schemas

--- app/models/manageiq/providers/lenovo/manager_mixin.rb
+++ app/models/manageiq/providers/lenovo/manager_mixin.rb
@@ -54,17 +54,20 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
         :fields => [
           {
             :component => 'sub-form',
+            :id        => 'endpoints-subform',
             :name      => 'endpoints-subform',
             :title     => _('Endpoints'),
             :fields    => [
               {
                 :component              => 'validate-provider-credentials',
+                :id                     => 'authentications.default.valid',
                 :name                   => 'authentications.default.valid',
                 :skipSubmit             => true,
                 :validationDependencies => %w[type],
                 :fields                 => [
                   {
                     :component  => "text-field",
+                    :id         => "endpoints.default.hostname",
                     :name       => "endpoints.default.hostname",
                     :label      => _("Hostname (or IPv4 or IPv6 address)"),
                     :isRequired => true,
@@ -72,6 +75,7 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
                   },
                   {
                     :component    => "text-field",
+                    :id           => "endpoints.default.port",
                     :name         => "endpoints.default.port",
                     :label        => _("API Port"),
                     :type         => "number",
@@ -81,6 +85,7 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
                   },
                   {
                     :component  => "text-field",
+                    :id         => "authentications.default.userid",
                     :name       => "authentications.default.userid",
                     :label      => "Username",
                     :isRequired => true,
@@ -88,6 +93,7 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
                   },
                   {
                     :component  => "password-field",
+                    :id         => "authentications.default.password",
                     :name       => "authentications.default.password",
                     :label      => "Password",
                     :type       => "password",
