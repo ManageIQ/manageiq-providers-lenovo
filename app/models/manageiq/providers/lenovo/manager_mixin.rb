dc87834a9602fb38991e954074a819c6515389be
Jason Frey <fryguy9@gmail.com>
2020-09-30 12:00:43 -0400

    Prefer ManageIQ::Password over legacy MiqPassword constant

--- app/models/manageiq/providers/lenovo/manager_mixin.rb
+++ app/models/manageiq/providers/lenovo/manager_mixin.rb
@@ -130,7 +130,7 @@ module ManageIQ::Providers::Lenovo::ManagerMixin
       hostname, port = endpoint&.values_at("hostname", "port")
       userid, password = authentication&.values_at("userid", "password")
 
-      password = MiqPassword.try_decrypt(password)
+      password = ManageIQ::Password.try_decrypt(password)
       password ||= find(args["id"]).authentication_password(endpoint_name) if args["id"]
 
       !!raw_connect(userid, password, hostname, port, "token", false, Vmdb::Appliance.USER_AGENT, true)
