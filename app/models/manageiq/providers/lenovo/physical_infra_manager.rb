58e349514ad3749186a40bbb5c45359cc290a87c
Oleg Barenboim <chessbyte@gmail.com>
2021-02-12 11:15:27 -0500

    Convert `supports_#{op}?` to `supports?(op)`
    Additionally, convert method definitions such as
    
    ```ruby
    def supports_port?
      true
    end
    ```
    
    to
    
    ```ruby
    supports :port
    ```

--- app/models/manageiq/providers/lenovo/physical_infra_manager.rb
+++ app/models/manageiq/providers/lenovo/physical_infra_manager.rb
@@ -15,6 +15,7 @@ class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::P
   require_nested :PhysicalSwitch
 
   supports :change_password
+  supports :provisioning
 
   def self.ems_type
     @ems_type ||= "lenovo_ph_infra".freeze
@@ -22,9 +23,5 @@ class ManageIQ::Providers::Lenovo::PhysicalInfraManager < ManageIQ::Providers::P
 
   def self.description
     @description ||= "Lenovo XClarity"
-  end
-
-  def supports_provisioning?
-    true
   end
 end
